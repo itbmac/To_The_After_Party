7ea95ff3a464901510a737ee23b0eef34db3a607
Gregory Rose <greg@ncroses.com>
2015-01-24 22:32:51 -0500

    Merge steven camera code

---
+++ TestMyo/Assets/Scripts/CameraEffects.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEffects : MonoBehaviour {
+
+	public Camera camera;
+	private CameraEntry cameraEntryScript;
+	private bool enabled = false;
+
+	// waviness
+	public float shakeAmount = 0.5f;
+	public float maxShakeAmount = 4.0f;
+	public float shakeIncreaseFactor = 0.4f;
+	public Vector3 timeFactors = new Vector3(5.0f,7.0f,11.0f);
+	private Vector3 originalEulerAngles;
+	public float slownessFactor = 10.0f;
+	public float maxSlownessFactor = 3.0f;
+	public float slownessReductionFactor = 0.05f;
+	// for the waviness (sine function)
+	private float shakeTime = 0.0f;
+
+	// blur
+	public float blurIntensity = 0.0f;
+	public float maxBlurIntensity = 0.6f;
+	public float blurIntensityFactor = 0.15f;
+	private MotionBlur blurScript;
+
+	// Use this for initialization
+	void Start () {
+		cameraEntryScript = camera.GetComponent<CameraEntry>();
+		blurScript = camera.GetComponent<MotionBlur>();
+		blurScript.blurAmount = 0.0f;
+		blurScript.enabled = true;
+		originalEulerAngles = camera.transform.localEulerAngles;
+		enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!enabled) {
+			if(cameraEntryScript.completed && Input.GetKeyDown(KeyCode.Space)) {
+				enabled = true;
+				originalEulerAngles = camera.transform.localEulerAngles;
+			}
+			return;
+		}
+		// shakiness
+		float devX = shakeAmount * Mathf.Sin (timeFactors.x * shakeTime / slownessFactor);
+		float devY = shakeAmount * Mathf.Sin (timeFactors.y * shakeTime / slownessFactor);
+		float devZ = shakeAmount * Mathf.Sin (timeFactors.z * shakeTime / slownessFactor);
+		camera.transform.localEulerAngles = originalEulerAngles + new Vector3(devX,devY,devZ);
+		shakeTime += Time.deltaTime;
+		shakeAmount += shakeIncreaseFactor * Time.deltaTime;
+		if (shakeAmount > maxShakeAmount) {
+			shakeAmount = maxShakeAmount;
+		}
+		slownessFactor -= slownessReductionFactor * Time.deltaTime;
+		if (slownessFactor < maxSlownessFactor) {
+			slownessFactor = maxSlownessFactor;
+		}
+
+		// blurriness
+		blurIntensity += blurIntensityFactor * Time.deltaTime;
+		blurScript.blurAmount = blurIntensity;
+	}
+}
---
+++ TestMyo/Assets/Scripts/CameraEntry.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEntry : MonoBehaviour {
+
+	public float MOVEMENT_SPEED = 1.0f;
+
+	public Transform startCube, endCube;
+	public Transform camera;
+	public Transform hands;
+
+	public bool completed = false;
+
+	// Use this for initialization
+	void Start () {
+		hands.localPosition = new Vector3 (0.0f, -1.0f, 0.345f);
+		hands.localEulerAngles = new Vector3 (60.0f, 0.0f, 0.0f);
+		camera.position = startCube.position;
+		camera.eulerAngles = startCube.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!completed) {
+			camera.position = new Vector3 (Mathf.Lerp (camera.position.x, endCube.position.x, MOVEMENT_SPEED * Time.deltaTime),
+			                               Mathf.Lerp (camera.position.y, endCube.position.y, MOVEMENT_SPEED * Time.deltaTime),
+			                               Mathf.Lerp (camera.position.z, endCube.position.z, MOVEMENT_SPEED * Time.deltaTime));
+			camera.eulerAngles = new Vector3 (Mathf.Lerp (camera.eulerAngles.x, endCube.eulerAngles.x, MOVEMENT_SPEED * Time.deltaTime),
+			                                  Mathf.Lerp (camera.eulerAngles.y, endCube.eulerAngles.y, MOVEMENT_SPEED * Time.deltaTime),
+			                                  Mathf.Lerp (camera.eulerAngles.z, endCube.eulerAngles.z, MOVEMENT_SPEED * Time.deltaTime));
+		}
+		if((camera.eulerAngles - endCube.eulerAngles).magnitude < 0.05) {
+			camera.eulerAngles = endCube.eulerAngles;
+			completed = true;
+		}
+		hands.localPosition = new Vector3 (0.0f,
+		                                   Mathf.Lerp (hands.localPosition.y, -0.229f, MOVEMENT_SPEED * Time.deltaTime),
+		                                   0.3f);
+		hands.localEulerAngles = new Vector3 (Mathf.Lerp (camera.localEulerAngles.x, -10.0f, MOVEMENT_SPEED * Time.deltaTime),
+		                                      0.0f,
+		                                      0.0f);
+	}
+}
