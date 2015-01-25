369e35579361e2658af10143c7d847d9fc666730
Steven Ong <szo@andrew.cmu.edu>
2015-01-24 23:18:22 -0500

    Reset command complete

---
+++ TestMyo/Assets/Scripts/ResetScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetScript : MonoBehaviour {
+
+	public Transform gameManager;
+	private GameManager gameManagerScript;
+
+	// Use this for initialization
+	void Start () {
+		gameManagerScript = gameManager.GetComponent<GameManager>();
+	}
+	
+	// If valid, we reset the entire game.
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && !gameManagerScript.GameRunning) {
+			Application.LoadLevel("Test Painting");
+		}
+	}
+}
